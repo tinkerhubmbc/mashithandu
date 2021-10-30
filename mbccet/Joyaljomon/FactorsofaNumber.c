8575ac7e67d1e8f8a43e4814433fde783707c9d1
Joseph DK <92791196+JD-thecoderK@users.noreply.github.com>
2021-10-30 22:39:45 +0530

    Update FactorsofaNumber.c

--- mbccet/Joyaljomon/FactorsofaNumber.c
+++ mbccet/Joyaljomon/FactorsofaNumber.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
-int main() {
+#include<stdio.h>
+int main() { //start of function
     int num, i;
     printf("Enter a positive integer: ");
-    scanf("%d", &num);
+    scanf("%d", &num); //Accepting integer input into a variable
     printf("Factors of %d are: ", num);
     for (i = 1; i <= num; ++i) {
         if (num % i == 0) {
-            printf("%d ", i);
+            printf("%d ", i);//printing numbers that can be used to divide the input integer
         }
     }
     return 0;
